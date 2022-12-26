a20092fb82f344dcb81a0c24bb7ccd7d1a918592
Sandrex233 <sandrex233@gmail.com>
2022-12-26 13:37:17 +0400

    next+tailwind

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+
+export default function Home() {
+  return (
+    <div className='line-through'>
+      Hello
+    </div>
+  )
+}
