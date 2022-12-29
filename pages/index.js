e49a415f0375c07b9a6c19017b0498c1dc61be6d
Sandrex233 <sandrex233@gmail.com>
2022-12-29 19:04:42 +0400

    styles

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
-
+import { AiOutlineClose } from 'react-icons/ai'
 
 const TodoPage = () => {
     const [isLoading, setIsLoading] = useState(true)
     const [todos, setTodos] = useState([])
     const [todo, setTodo] = useState('');
+    const [theme, setTheme] = useState(false);
 
     // fetching todos
     useEffect(() => {
@@ -56,8 +57,12 @@ const TodoPage = () => {
         makeRequest()
     }
 
+    const themeToggle = () => {
+        setTheme(!theme)
+    }
+
     return (
-        <div>
+        <div className='bg-[#161722] h-screen flex justify-center items-center flex-col'>
             <div className='flex flex-row'>
                 <input
                     type="text"
@@ -72,16 +77,21 @@ const TodoPage = () => {
                     submit todos
                 </button>
             </div>
-            {todos.map((todo) => {
-                return (
-                    <div key={todo.id}>
-                        {todo.id} {todo.text}
-                        <button
-                            onClick={() => deleteTodo(todo.id)}
-                            className='ml-5 class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"'>Delete</button>
-                    </div>)
-            })}
-            <h2>{todos.length}</h2>
+            <div className='mt-4 max-w-md w-[27em] px-4 bg-[#25273c] rounded-md shadow-2xl'>
+                {todos.map((todo) => {
+                    return (
+                        <div key={todo.id} className='border-b py-5 border-[#393a4c] justify-between flex text-[#d2d3db] max-w-xl font-normal'>
+                            {todo.id} {todo.text}
+                            <button
+                                onClick={() => deleteTodo(todo.id)}
+                                className='ml-5 px-6  text-[#484b6a] text-lg'><AiOutlineClose /></button>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className='mt-4 max-w-md w-[27em] bg-[#25273c] rounded-md shadow-2xl'>
+                <h2>{todos.length}</h2>
+            </div>
         </div>
     )
 }
--- style-guide.md
+++ style-guide.md
@@ -18,21 +18,21 @@ The designs were created to the following widths:
 
 ### Light Theme
 
-- Very Light Gray: hsl(0, 0%, 98%)
-- Very Light Grayish Blue: hsl(236, 33%, 92%)
-- Light Grayish Blue: hsl(233, 11%, 84%)
-- Dark Grayish Blue: hsl(236, 9%, 61%)
-- Very Dark Grayish Blue: hsl(235, 19%, 35%)
+- Very Light Gray: hsl(0, 0%, 98%) // #fafafa
+- Very Light Grayish Blue: hsl(236, 33%, 92%) // #e4e5f1
+- Light Grayish Blue: hsl(233, 11%, 84%) // #d2d3db
+- Dark Grayish Blue: hsl(236, 9%, 61%) // #9394a5
+- Very Dark Grayish Blue: hsl(235, 19%, 35%) // #484b6a
 
 ### Dark Theme
 
-- Very Dark Blue: hsl(235, 21%, 11%)
-- Very Dark Desaturated Blue: hsl(235, 24%, 19%)
-- Light Grayish Blue: hsl(234, 39%, 85%)
-- Light Grayish Blue (hover): hsl(236, 33%, 92%)
-- Dark Grayish Blue: hsl(234, 11%, 52%)
-- Very Dark Grayish Blue: hsl(233, 14%, 35%)
-- Very Dark Grayish Blue: hsl(237, 14%, 26%)
+- Very Dark Blue: hsl(235, 21%, 11%) // #161722
+- Very Dark Desaturated Blue: hsl(235, 24%, 19%) // #25273c
+- Light Grayish Blue: hsl(234, 39%, 85%) // #25273c
+- Light Grayish Blue (hover): hsl(236, 33%, 92%) // #e4e5f1
+- Dark Grayish Blue: hsl(234, 11%, 52%) // #777a92
+- Very Dark Grayish Blue: hsl(233, 14%, 35%) // #4d5066
+- Very Dark Grayish Blue: hsl(237, 14%, 26%) // #393a4c
 
 ## Typography
 
